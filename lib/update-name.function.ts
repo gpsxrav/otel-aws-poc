6d7c55b41caf4cc815a3ee751351546d17a86022
gdw4man <Hariharan.Manoharan@csaa.com>
2023-06-07 09:27:29 -0700

    Initial commit

--- lib/update-name.function.ts
+++ lib/update-name.function.ts
@@ -20,44 +20,7 @@ const tracer = trace.getTracer('my-service-tracer')
 const ddbClient = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
 
-// Acts like a decorator, helps reduce nesting that comes from 'startActiveSpan'
-function spanify<F extends (args: Parameters<F>[0]) => ReturnType<F>>(
-  fn: F,
-  spanOptions: SpanOptions = {},
-  spanName: string = fn.name.replace(/^_/, '')
-): (args: Omit<Parameters<F>[0], 'span'>) => Promise<ReturnType<F>> {
-  return (functionArgs) => {
-    return tracer.startActiveSpan(spanName, spanOptions, async (span: Span): Promise<ReturnType<F>> => {
-      try {
-        const result = await fn({ ...functionArgs, span })
-
-        span.setStatus({
-          code: SpanStatusCode.OK,
-        })
-
-        return result
-      } catch (error: any) {
-        const errorMessage = String(error)
-
-        if (!error.spanExceptionRecorded) {
-          span.recordException(error)
-          span.setStatus({ code: SpanStatusCode.ERROR, message: errorMessage })
-          error.spanExceptionRecorded = true
-        }
-
-        throw error
-      } finally {
-        span.end()
-      }
-    })
-  }
-}
-
-
-
-async function updateName(name: string, span: Span): Promise<number> {
-
-  span.setAttributes({ name })
+async function updateName(name: string): Promise<number> {
 
   let id = getRandomInt(10000);
   await ddbDocClient.send(new PutCommand({
@@ -75,25 +38,15 @@ function getRandomInt(max: number) {
     return Math.floor(Math.random() * max);
 }
 
-const handler = async (event: APIGatewayEvent, reqContext: Context): Promise<APIGatewayProxyResult> => {
+const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
   let span: Span;
   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-  console.log(`Context: ${JSON.stringify(reqContext, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
 
   let eventJson = JSON.parse(JSON.stringify(event, null, 2));
-  let reqContextJson = JSON.parse(JSON.stringify(reqContext, null, 2));
-  const propagator = new W3CTraceContextPropagator();
+  let reqContextJson = JSON.parse(JSON.stringify(context, null, 2));
 
-  const parentCtx = propagator.extract(ROOT_CONTEXT, event.headers.carrier, defaultTextMapGetter);
-
-  if (!event.headers.traceparent) {
-    span = tracer.startActiveSpan('update-name-api', { root: true });
-  }else {
-    span = tracer.startSpan("update-name-api", undefined, parentCtx);
-  }
-
-  console.log('parentCtx - '+parentCtx)
-    if (!eventJson.body) {
+  if (!eventJson.body) {
         return {
             statusCode: 500,
             body: JSON.stringify({
@@ -107,7 +60,7 @@ const handler = async (event: APIGatewayEvent, reqContext: Context): Promise<API
     console.log('body - '+ JSON.stringify(body))
     let name = body.name;
     console.log('name - '+ name)
-    const newId = await updateName(name, span)
+    const newId = await updateName(name)
 
     const response = {
       statusCode: 200,
@@ -116,11 +69,9 @@ const handler = async (event: APIGatewayEvent, reqContext: Context): Promise<API
       }),
     };
 
-    span.end()
     return response
 
 };
 
-// Must export via handler, required due to otel manipulating lambda entry point
-//  https://github.com/open-telemetry/opentelemetry-js/issues/1946
+
 module.exports = { handler }
